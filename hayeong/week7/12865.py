bc687a62ccef06c3b03372f849493a086535deef
hayeongKo <polarpheno@icloud.com>
2024-08-30 15:20:49 +0900

    [FEAT] add 17865.py

---
+++ hayeong/week7/12865.py
@@ -0,0 +1,27 @@
+# 12865
+# 평범한 배낭
+
+import sys
+
+items = [[0, 0]]
+input = sys.stdin.readline
+
+N, K = map(int, input().split())
+
+# 가로: value, 세로: item_index
+dp = [[0] * (K+1) for _ in range(N+1)]
+
+for _ in range(N):
+    items.append(list(map(int, input().split())))
+
+for i in range(1, N+1): # item index
+    for j in range(1, K+1):  # weight
+        W = items[i][0]
+        V = items[i][1]
+
+        if j < W:
+            dp[i][j] = dp[i-1][j]
+        else:
+            dp[i][j] = max(dp[i-1][j], dp[i-1][j-W]+V)
+
+print(dp[N][K])
