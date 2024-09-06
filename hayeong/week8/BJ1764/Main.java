f03d86e928d22684c81919f505070c4a11263968
hayeongKo <polarpheno@icloud.com>
2024-09-07 02:11:40 +0900

    [FEAT] add 1764

---
+++ hayeong/week8/BJ1764/Main.java
@@ -0,0 +1,38 @@
+package BJ1764;
+
+// 1764
+// 듣보잡
+
+import java.io.*;
+import java.util.*;
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        String[] inputLst = br.readLine().split(" ");
+
+        int n = Integer.parseInt(inputLst[0]);
+        int m = Integer.parseInt(inputLst[1]);
+
+        HashSet<String> listeners = new HashSet<>(); // 듣도 못한 사람
+        List<String> answer = new ArrayList<>();
+
+        for(int i = 0; i < n; i++) {
+            listeners.add(br.readLine());
+        }
+
+        for(int i = 0; i < m; i++) {
+            String looker = br.readLine(); // 보도 못한 사람 비교
+            if (listeners.contains(looker)) {
+                answer.add(looker);
+            }
+        }
+
+        Collections.sort(answer);
+
+        System.out.println(answer.size());
+        for (String name:answer) {
+            System.out.println(name);
+        }
+    }
+}
